28afc7b26b0db6873df55b93f193058a0c84d735
bakedsnake <gilferrandm@gmail.com>
2026-01-13 19:19:15 +0100

    add flags

--- pv.c
+++ pv.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <getopt.h>
 #include <math.h>
 #include <signal.h>
 #include <sys/stat.h>
@@ -7,6 +8,18 @@
 #include <pipewire/pipewire.h>
 #include <sndfile.h>
 
+static char version[5] = "0.0.2";
+
+static struct option options[] = {
+  {"help",              no_argument, 		0, 'h'},
+  {"version",           no_argument, 		0, 'v'},
+  {"format",            required_argument, 	0, 'f'},
+  {"rate",		required_argument, 	0, 'r'},
+  {"output", 		required_argument, 	0, 'o'},
+  {"target", 		required_argument, 	0, 't'},
+  {0, 			0, 			0,  0 }
+};
+
 struct data {
         struct pw_main_loop *loop;
         struct pw_stream *stream;
@@ -14,6 +27,7 @@ struct data {
         struct spa_audio_info format;
         unsigned move:1;
         SNDFILE *sf;
+	char *sfName;
 };
 
 static void on_process(void *userdata)
@@ -38,7 +52,7 @@ static void on_process(void *userdata)
         /* move cursor up */
         if (data->move)
                 fprintf(stdout, "%c[%dA", 0x1b, n_channels + 1);
-        fprintf(stdout, "captured %d samples\n", n_samples / n_channels);
+        fprintf(stdout, "\n");
         for (c = 0; c < data->format.info.raw.channels; c++) {
                 max = 0.0f;
                 for (n = c; n < n_samples; n += n_channels) {
@@ -56,10 +70,10 @@ static void on_process(void *userdata)
         sf_write_float(data->sf, samples, n_samples);
 
         // Keep track of file size.
-        FILE *file = fopen("out-recording.wav", "r");
+        FILE *file = fopen(data->sfName, "r");
         fseek(file, 0, SEEK_END);
         long fsize = ftell(file);
-        fprintf(stdout, "File size: %ld bytes", fsize);
+        fprintf(stdout, "File: %s | %ld bytes", data->sfName, fsize);
         rewind(file);
 
         data->move = true;
@@ -90,7 +104,7 @@ on_stream_param_changed(void *_data, uint32_t id, const struct spa_pod *param)
         /* call a helper function to parse the format for us. */
         spa_format_audio_raw_parse(param, &data->format.info.raw);
 
-        fprintf(stdout, "capturing rate:%d channels:%d\n",
+        fprintf(stdout, "source rate:%d channels:%d\n",
                         data->format.info.raw.rate, data->format.info.raw.channels);
 
 }
@@ -109,22 +123,48 @@ static void do_quit(void *userdata, int signal_number)
 
 int main(int argc, char *argv[])
 {
+        int opt;
+	char* rateStr = NULL;
+	char* streamTarget = NULL;
+	char* path = NULL;
+
+        while ((opt = getopt_long(argc, argv, "hvfrot:", options, NULL)) != -1) {
+                switch (opt) {
+                        case 'h':
+                                printf("Usage: ...\n");
+                                return 0;
+                        case 'v':
+                                printf("Version: %s\n", version);
+                                return 0;
+                        case 'o':
+                                path = optarg;
+                                break;
+                        case 'r':
+                                rateStr = optarg;
+                        case 't':
+                                streamTarget = optarg;
+                        case '?':
+                                default:
+                                break;
+                }
+        }
+
         struct data data = { 0, };
         const struct spa_pod *params[1];
         uint8_t buffer[1024];
         struct pw_properties *props;
         struct spa_pod_builder b = SPA_POD_BUILDER_INIT(buffer, sizeof(buffer));
 
-        const char *path = "out-recording.wav";
+        data.sfName = path != NULL ? path : "out-recording.wav";
         const int channels = 2;
-        const int samplerate = 48000;
+        const int samplerate = rateStr != NULL ? atoi(rateStr) : 48000;
         const int frames = samplerate;
 
         SF_INFO sfinfo = {0};
         sfinfo.samplerate = samplerate;
         sfinfo.channels = channels;
         sfinfo.format = SF_FORMAT_WAV | SF_FORMAT_PCM_32;
-        data.sf = sf_open(path, SFM_WRITE, &sfinfo);
+        data.sf = sf_open(data.sfName, SFM_WRITE, &sfinfo);
         if (!data.sf) {
             fprintf(stderr, "Error opening file: %s\n", sf_strerror(NULL));
             return 1;
@@ -154,9 +194,9 @@ int main(int argc, char *argv[])
                         PW_KEY_MEDIA_CATEGORY, "Capture",
                         PW_KEY_MEDIA_ROLE, "Music",
                         NULL);
-        if (argc > 1)
+        if (argc > 1 && streamTarget != NULL)
                 /* Set stream target if given on command line */
-                pw_properties_set(props, PW_KEY_TARGET_OBJECT, argv[1]);
+                pw_properties_set(props, PW_KEY_TARGET_OBJECT, streamTarget);
 
         /* uncomment if you want to capture from the sink monitor ports */
         /* pw_properties_set(props, PW_KEY_STREAM_CAPTURE_SINK, "true"); */
